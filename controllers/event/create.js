9b652444b342e006bef164cffbae1191f6da3720
Duncan Giles <duncan3142@gmail.com>
2019-05-29 12:35:10 +0100

    Add media selection to event form

--- controllers/event/create.js
+++ controllers/event/create.js
@@ -1,4 +1,4 @@
-function imageQuery(Media, mimeFilter) {
+function mediaQuery(Media, mimeFilter) {
 	return Media
 		.query()
 		.select([
@@ -11,24 +11,15 @@ function imageQuery(Media, mimeFilter) {
 		.orderBy(['type', 'name']);
 }
 
-function documentQuery(Media, mimeFilter) {
-	return Media
-		.query()
-		.select([
-			'id',
-			'name',
-			'description',
-			'type'
-		])
-		.whereIn('type', mimeFilter)
-		.orderBy(['type', 'name']);
+function mediaQueries(Media, mimeFilters) {
+	return Promise.all([mediaQuery(Media, mimeFilters.images), mediaQuery(Media, mimeFilters.documents)]);
 }
 
 function get(modelRegistry, mimeFilters) {
 	return async function (req, res) {
 		const Media = modelRegistry.get('media');
-		const [images, docs] = await Promise.all([imageQuery(Media, mimeFilters.images), documentQuery(Media, mimeFilters.documents)]);
-		res.render('event_form', {title: 'Create event', images, docs});
+		const [images, flyers] = await mediaQueries(Media, mimeFilters);
+		res.render('event_form', {title: 'Create event', images, flyers});
 	};
 }
 
@@ -38,7 +29,9 @@ function eventInsertQuery(Event, event) {
 		.insert(event);
 }
 
-async function insertEvent(Event, event, res, next) {
+async function insertEvent(Event, event, routeHandles) {
+	const res = routeHandles.res;
+	const next = routeHandles.next;
 	try {
 		await eventInsertQuery(Event, event);
 		res.redirect('/');
@@ -47,16 +40,17 @@ async function insertEvent(Event, event, res, next) {
 	}
 }
 
-function checkSelected(available, selectedID) {
+function markSelected(available, selectedID) {
 	return available.map(elem => {
-		if (elem.id === selectedID) {
-			elem.checked = 'true';
-		}
+		elem.selected = (elem.id === selectedID);
 		return elem;
 	});
 }
 
-async function validateEventCreate(errors, models, req, res, next) {
+async function validateEventCreate(errors, models, mimeFilters, routeHandles) {
+
+	const req = routeHandles.req;
+	const res = routeHandles.res;
 
 	const event = {
 		title: req.body.title,
@@ -69,16 +63,17 @@ async function validateEventCreate(errors, models, req, res, next) {
 	};
 
 	if (errors.isEmpty()) {
-		await insertEvent(models.Event, event, res, next);
+		await insertEvent(models.Event, event, routeHandles);
 	} else {
 		// There are errors. Render the form again with sanitized values/error messages.
-		const availableImages = await imageQuery(models.Media);
-		const checkedImages = checkSelected(availableImages, event.images);
-		res.render('event_form', { title: 'Create event', event, checkedImages, errors: errors.mapped() });
+		const [availableImages, availableFlyers] = await mediaQueries(models.Media, mimeFilters);
+		const images = markSelected(availableImages, event.imageID);
+		const flyers = markSelected(availableFlyers, event.flyerID);
+		res.render('event_form', { title: 'Create event', event, images, flyers, errors: errors.mapped() });
 	}
 }
 
-function createEvent(modelRegistry, validationResult) {
+function createEvent(modelRegistry, validationResult, mimeFilters) {
 	return async (req, res, next) => {
 
 		// Extract the validation errors from a request.
@@ -88,71 +83,72 @@ function createEvent(modelRegistry, validationResult) {
 			Media: modelRegistry.get('media')
 		};
 
-		await validateEventCreate(errors, models, req, res, next);
+		await validateEventCreate(errors, models, mimeFilters, { req, res, next });
 	};
 }
 
 const startBeforeEnd = (start, { req }) => (start <= req.body.end);
 
-// function coerceImages(req, res, next) {
-// 	if (!Array.isArray(req.body.images)) {
-// 		if (req.body.images) {
-// 			req.body.images = new Array(req.body.images);
-// 		} else {
-// 			req.body.images = [];
-// 		}
-// 	}
-// 	next();
-// }
+async function checkMediaExists(Media, mediaID, mimeFilter) {
+	const media = await Media
+		.query()
+		.select(['id'])
+		.where('id', mediaID)
+		.whereIn('type', mimeFilter)
+		.first();
+	return media ? media.id === mediaID : false;
+}
 
-function post(validators, modelRegistry) {
+function validMediaID(Media, mimeFilter) {
+	return async function (mediaID) {
+		return mediaID < 0 ? true : await checkMediaExists(Media, mediaID, mimeFilter);
+	};
+}
+
+function post(validators, modelRegistry, mimeFilters) {
 
 	const bodyValidator = validators.body;
 	const validationResult = validators.result;
+	const Media = modelRegistry.get('media');
 
 	return [
 
-		// coerceImages,
-
 		bodyValidator.check('title', 'Event title required').isLength({ min: 1 }).trim(),
-
-		bodyValidator.check('description', 'Event description required').isLength({ min: 1 }).trim(),
-
-		bodyValidator.check('when', 'Event date required').isLength({ min: 1 }).trim(),
-
-		bodyValidator.check('location', 'Event location required').isLength({ min: 1 }).trim(),
-
-		bodyValidator.check('start', 'Event start required').isISO8601({ strict: true }),
-
-		bodyValidator.check('end', 'Event end required').isISO8601({ strict: true }),
-
 		bodyValidator.filter('title').trim(),
 
+
+		bodyValidator.check('description', 'Event description required').isLength({ min: 1 }).trim(),
 		bodyValidator.filter('description').trim(),
 
+		bodyValidator.check('when', 'Event date required').isLength({ min: 1 }).trim(),
 		bodyValidator.filter('when').trim(),
 
+		bodyValidator.check('location', 'Event location required').isLength({ min: 1 }).trim(),
 		bodyValidator.filter('location').trim(),
 
+		bodyValidator.check('start', 'Event start required').isISO8601({ strict: true }),
 		bodyValidator.filter('start').toDate(),
-
+		bodyValidator.check('end', 'Event end required').isISO8601({ strict: true }),
 		bodyValidator.filter('end').toDate(),
-
-		// bodyValidator.filter('image.*').escape(),
-
-		// bodyValidator.filter('document.*').escape(),
-
 		bodyValidator.check('start', 'Start date must not come after end date').custom(startBeforeEnd),
 
+		bodyValidator.check('image', 'Image ID must be an integer').isInt(),
+		bodyValidator.filter('image').toInt(),
+		bodyValidator.check('image', 'Image ID must exists').custom(validMediaID(Media, mimeFilters.images)),
+
+		bodyValidator.check('flyer', 'Flyer ID must be an integer').isInt(),
+		bodyValidator.filter('flyer').toInt(),
+		bodyValidator.check('flyer', 'Flyer ID must exists').custom(validMediaID(Media, mimeFilters.documents)),
+
 		// Process request after validation and sanitization.
-		createEvent(modelRegistry, validationResult)
+		createEvent(modelRegistry, validationResult, mimeFilters)
 	];
 }
 
 function controller(validators, modelRegistry, mimeFilters) {
 	return {
 		get: get(modelRegistry, mimeFilters),
-		post: post(validators, modelRegistry)
+		post: post(validators, modelRegistry, mimeFilters)
 	};
 }
 
--- db/populate.js
+++ db/populate.js
@@ -18,8 +18,8 @@ function eventInsert(db, title, description, when, location, start, end) {
 	return db('event').insert({title, description, when, location, start, end}).returning('id');
 }
 
-function eventMediaInsert(db, event_id, media_id) {
-	return db('event_media').insert({event_id, media_id});
+function eventMediaInsert(db, event_id, media_id, usage) {
+	return db('event_media').insert({event_id, media_id, usage});
 }
 
 function deleteData(db) {
@@ -31,19 +31,21 @@ async function populate(db) {
 		await deleteData(db);
 		const [mediaIDs, eventIDs] = await Promise.all([
 			Promise.all([
-				mediaInsert(db, 'A bridge flyer', 'bridgeflyer.pdf', 'image', 'committee.jpg'),
-				mediaInsert(db, 'A sale application form', 'saleApplication.docx', 'image', 'header.jpg')
+				mediaInsert(db, 'A committee image', 'committee.jpg', 'image/jpeg', 'committee.jpg'),
+				mediaInsert(db, 'A header image', 'header.jpg', 'image/jpeg', 'header.jpg'),
+				mediaInsert(db, 'A guide document', 'guide.pdf', 'application/pdf', 'guide.pdf')
 			]),
 			Promise.all([
-				eventInsert(db, 'Coffee morn', 'A coffee morning', 'May 12th', 'Tower House', new Date(2019, 4, 12), new Date(2019, 4, 12)),
-				eventInsert(db, 'Bridge', 'A bridge game', 'June 1st', 'Jan Wright\'s', new Date(2019, 5, 1), new Date(2019, 5, 1)),
-				eventInsert(db, 'Garden sale','A garden plant sale', 'July 23rd - July 25th', 'Sheen Lane Centre', new Date(2019, 6, 23),new Date(2019, 6, 25))
+				eventInsert(db, 'Coffee morn', 'A coffee morning', 'May 12th', 'Tower House', new Date(2019, 7, 12), new Date(2019, 7, 12)),
+				eventInsert(db, 'Bridge', 'A bridge game', 'June 1st', 'Jan Wright\'s', new Date(2019, 8, 1), new Date(2019, 8, 1)),
+				eventInsert(db, 'Garden sale','A garden plant sale', 'July 23rd - July 25th', 'Sheen Lane Centre', new Date(2019, 9, 23),new Date(2019, 9, 25))
 			])
 		]);
 		await Promise.all([
-			eventMediaInsert(db, eventIDs[1][0], mediaIDs[0][0]),
-			eventMediaInsert(db, eventIDs[2][0], mediaIDs[0][0]),
-			eventMediaInsert(db, eventIDs[2][0], mediaIDs[1][0])
+			eventMediaInsert(db, eventIDs[0][0], mediaIDs[2][0], 'flyer'),
+			eventMediaInsert(db, eventIDs[1][0], mediaIDs[0][0], 'image'),
+			eventMediaInsert(db, eventIDs[2][0], mediaIDs[0][0], 'image'),
+			eventMediaInsert(db, eventIDs[2][0], mediaIDs[1][0], 'image')
 		]);
 	} catch(err) {
 		console.log(err);
