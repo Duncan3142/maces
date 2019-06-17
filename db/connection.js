078cd548917afee6a5e91472e4b1bda2c12d2481
Duncan Giles <duncan3142@gmail.com>
2019-06-17 16:43:40 +0100

    Database connection url

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,6 @@
 module.exports = {
 	client: 'pg',
-	connection: {
-		host: process.env.POSTGRES_HOST || "localhost",
-		port: process.env.POSTGRES_PORT || 5432,
-		database: "maces",
-		user: process.env.POSTGRES_USERNAME,
-		password: process.env.POSTGRES_PASSWORD
-	},
+	connection: process.env.DATABASE_URL,
 	pool: {
 		min: 1,
 		max: 16
